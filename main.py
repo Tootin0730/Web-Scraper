8f496e328976d6fa2b8e188d18606d18f4798d45
Tootin0730 <yisan1004@gmail.com>
2023-04-14 11:45:21 -0400

    2.9 Default parameter

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-def tax_calculator(money):
-    print("Your tax is: $", money * 0.35)
+def plus(a=0, b=0):
+    print(a + b)
 
-tax_calculator(4200)
-tax_calculator(3000)
+def minus(a=0, b=0):
+    print(a - b)
+
+def multiply(a=0, b=0):
+    print(a * b)
+
+def divide(a=0, b=1):
+    print(a / b)
+
+def power(a=0, b=1):
+    print(a ** b)
+
+plus(3, 4)
+minus(5, 3)
+multiply(2, 8)
+divide(6, 3)
+power(3, 3)
+
+plus()
+minus()
+multiply()
+divide()
+power()
