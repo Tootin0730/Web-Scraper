f8d06b8176cbfb6a228c2335f527a63ebd041e04
Tootin0730 <yisan1004@gmail.com>
2023-04-15 11:35:58 -0400

    3.1 Else & Elif

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-a = "San"
+winner = 5
 
-if a == "San":
-    print("True")
+if winner > 10:
+    print("Winner is greater than 10.")
+elif winner < 10:
+    print("Winner is less than 10.")
+else:
+    print("Winner is exactly 10.")
