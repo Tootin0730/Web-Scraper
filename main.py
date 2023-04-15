cdcf34ddf9ce36a9c8401aa846b7745db58e9a7a
Tootin0730 <yisan1004@gmail.com>
2023-04-15 11:34:01 -0400

    3.0 If

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-def make_juice(fruit):
-    return f"{fruit} + juice"
+a = "San"
 
-def add_ice(juice):
-    return f"{juice} + ice"
-
-def add_sugar(iced_juice):
-    return f"{iced_juice} + sugar"
-
-juice = "apple"
-cold_juice = add_ice(juice)
-perfect_juice = add_sugar(cold_juice)
-
-print(perfect_juice, "is the perfect juice")
+if a == "San":
+    print("True")
