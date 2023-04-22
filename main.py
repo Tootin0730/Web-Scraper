109446c29701f041115e1a55dfe3ceb7935c3208
Tootin0730 <yisan1004@gmail.com>
2023-04-22 09:31:09 -0400

    4.6 URL Formatting

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 websites = {
-    "Google.com",
-    "Airbnb.com",
-    "Twitter.com",
-    "Facebook.com",
-    "Tiktok.com"
+    "google.com",
+    "https://airbnb.com",
+    "twitter.com",
+    "https://facebook.com",
+    "tiktok.com"
 }
 
 for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
     print(website)
