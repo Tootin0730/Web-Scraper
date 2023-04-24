38abea38be4ed9b743d16a4bed4161b8061418a3
Tootin0730 <yisan1004@gmail.com>
2023-04-24 09:50:32 -0400

    4.8 Status Codes

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ websites = {
     "tiktok.com"
 }
 
+results = {}
+
 for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
+    response = get(website)
+    if response.status_code == 200:
+        results[website] = "OK"
+    else:
+        results[website] = "FAILED"
+        
     print(website)
