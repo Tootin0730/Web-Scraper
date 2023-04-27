f36aad9314f96c5862ec17595c16eb7ed150aad7
Tootin0730 <yisan1004@gmail.com>
2023-04-27 10:14:15 -0400

    5.3 Initial Request
    5.1~5.2 were introduction and recap lectures with no code changes.

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 from requests import get
 # installed requests
-websites = {
-    "google.com",
-    "https://airbnb.com",
-    "twitter.com",
-    "https://facebook.com",
-    "tiktok.com"
-}
+base_url = "https://weworkremotely.com/remote-jobs/search?&term="
+search_term = "python"
 
-results = {}
-
-for website in websites:
-    if not website.startswith("https://"):
-        website = f"https://{website}"
-    response = get(website)
-    if response.status_code == 200:
-        results[website] = "OK"
-    else:
-        results[website] = "FAILED"
-        
-    print(results)
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    print(response.text)
