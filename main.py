f1b2812291b0c4d96ce0d61beaa9084a238d59d7
Tootin0730 <yisan1004@gmail.com>
2023-04-29 12:02:30 -0400

    5.8 Saving Results

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from bs4 import BeautifulSoup
 
 main_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
+
 response = get(f"{main_url}{search_term}")
 if not response.status_code == 200:
     print("Can't request the website!")
 else:
+    results = []
     soup = BeautifulSoup(response.text ,"html.parser")
     jobs = soup.find_all('section', class_="jobs")
-    for job in jobs:
-        job_post = job.find_all("li")
+    for job_section in jobs:
+        job_post = job_section.find_all("li")
         job_post.pop(-1)
         for post in job_post:
             anchors = post.find_all('a')
@@ -18,6 +20,12 @@ else:
             link = anchor['href']
             company, kind, region = anchor.find_all('span', class_="company")
             title = anchor.find('span', class_='title')
-            print(company, kind, region, title)
-            print("/////////////////////")
-            print("/////////////////////")
+            job_data = {
+                'company' : company.string,
+                'region' : region.string,
+                'position' : title.string,
+            }
+            results.append(job_data)
+    for result in results:
+        print(result)
+        print("//////////")
