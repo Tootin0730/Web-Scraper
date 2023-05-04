4304377fb604a474738b715df4ecf94acf875a96
Tootin0730 <yisan1004@gmail.com>
2023-05-04 13:44:43 -0400

    5.11 Recursive

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from requests import get
 from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-jobs= extract_wwr_jobs("python")
-print(jobs)
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+browser = webdriver.Chrome(options=options)
+
+base_url = "https://kr.indeed.com/jobs"
+search_term = "python"
+
+browser.get(f"{base_url}?q={search_term}")
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul",class_="jobsearch-ResultsList")
+#print("job_list? ",job_list)
+jobs = job_list.find_all('li', recursive=False)
+print(len(jobs))
+print("//////")
+
+while (True):
+    pass
