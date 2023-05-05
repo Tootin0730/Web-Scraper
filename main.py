397195c651d82e43daa0d9348c52b78c16101086
Tootin0730 <yisan1004@gmail.com>
2023-05-05 13:16:30 -0400

    5.13 None

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from bs4 import BeautifulSoup
 options = Options()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+
 browser = webdriver.Chrome(options=options)
 
-base_url = "https://kr.indeed.com/jobs"
-search_term = "python"
+browser.get("https://kr.indeed.com/jobs?q=python")
 
-browser.get(f"{base_url}?q={search_term}")
 soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul",class_="jobsearch-ResultsList")
-#print("job_list? ",job_list)
-jobs = job_list.find_all('li', recursive=False)
-print(len(jobs))
-print("//////")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
 
-while (True):
-    pass
+
+jobs = job_list.find_all('li', recursive=False)
+
+for job in jobs:
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        print("job li")
+    else:
+        print("mosaic li")
