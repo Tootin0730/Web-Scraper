6e64f5a450246781eef1f17ff15341b101959cb1
Tootin0730 <yisan1004@gmail.com>
2023-05-06 16:19:01 -0400

    5.14 Select

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
+from requests import get
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
+
 options = Options()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 browser = webdriver.Chrome(options=options)
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
 
-browser.get("https://kr.indeed.com/jobs?q=python")
+browser.get(f"{base_url}{search_term}")
 
+results = []
 soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul", class_="jobsearch-ResultsList")
-
-
-jobs = job_list.find_all('li', recursive=False)
-
+job_list = soup.find('ul', class_="jobsearch-ResultsList")
+jobs = job_list.find_all('li', resulsive=False)
 for job in jobs:
     zone = job.find("div", class_="mosaic-zone")
-    if zone == None:
-        print("job li")
-    else:
-        print("mosaic li")
+if zone == None:
+    h2 = job.find("h2", class_="jobTitle")
+    anchor = job.select_one("h2 a")
+    if anchor != None:
+        title = anchor["aria-label"]
+        link = anchor["href"]
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
+        job_data = {
+            'link': f"https://kr.indeed.com{link}",
+            'company': company.string,
+            'location': location.string,
+            'position': title,
+        }
+        results.append(job_data)
+
+
+for result in results:
+    print(result, "\n/////////\n")
