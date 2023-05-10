babf85e4309f0ad8bbd3ac58c6e92e533ead7a92
Tootin0730 <yisan1004@gmail.com>
2023-05-09 21:22:31 -0400

    5.15 Pages pt.1

--- main.py
+++ main.py
@@ -2,40 +2,50 @@ from requests import get
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
 
 options = Options()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-
 browser = webdriver.Chrome(options=options)
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
 
-browser.get(f"{base_url}{search_term}")
+def get_page_count(keyword):
+    base_url = "https://kr.indeed.com/jobs?q="
+    end_url = "&limit=50"
+    browser.get(f"{base_url}{keyword}{end_url}")
 
-results = []
-soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find('ul', class_="jobsearch-ResultsList")
-jobs = job_list.find_all('li', resulsive=False)
-for job in jobs:
-    zone = job.find("div", class_="mosaic-zone")
-if zone == None:
-    h2 = job.find("h2", class_="jobTitle")
-    anchor = job.select_one("h2 a")
-    if anchor != None:
-        title = anchor["aria-label"]
-        link = anchor["href"]
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination = soup.find("nav", class_="ecydgvn0")
+    if pagination == None:
+        return 1
+    pages = pagination.find_all("div", recursive=False)
+    print(len(pages))
+
+get_page_count("nest")
+
+
+def extract_indeed_jobs(keyword):
+    base_url = "https://kr.indeed.com/jobs?q="
+    end_url = "&limit=50"
+    browser.get(f"{base_url}{keyword}{end_url}")
+
+    results = []
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    jobs = job_list.find_all('li', recursive=False)
+    for job in jobs:
+        zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        anchor = job.select_one("h2 a")
+        title = anchor['aria-label']
+        link = anchor['href']
         company = job.find("span", class_="companyName")
         location = job.find("div", class_="companyLocation")
         job_data = {
-            'link': f"https://kr.indeed.com{link}",
-            'company': company.string,
-            'location': location.string,
-            'position': title,
+            'link' : f"http://kr.indeed.com{link}",
+            'company' : company.string,
+            'location' : location.string,
+            'position' : title
         }
-        results.append(job_data)
-
-
-for result in results:
-    print(result, "\n/////////\n")
+    results.append(job_data)
+    for result in results:
+        print(result, "\n//////////\n")
