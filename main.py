75ace55627795fbd9e909d8c7bf1a39a382ca0f4
Tootin0730 <yisan1004@gmail.com>
2023-05-10 19:01:07 -0400

    5.17 Refactor

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-browser = webdriver.Chrome(options=options)
+from bs4 import BeautifulSoup
 
 def get_page_count(keyword):
-    base_url = "https://kr.indeed.com/jobs?q="
-    browser.get(f"{base_url}{keyword}")
-    soup = BeautifulSoup(browser.page_source, "html.parser")
-    pagination = soup.find("nav", class_="ecydgvn0")
-    pages = pagination.find_all("div", class_="ecydgvn1")
-    if not pages:
+    driver = webdriver.Chrome()
+    driver.get(f'https://kr.indeed.com/jobs?q={keyword}')
+    soup=BeautifulSoup(driver.page_source, 'html.parser')
+    pagination = soup.find('nav', class_='css-jbuxu0')
+    if pagination == None:
         return 1
+    pages = pagination.find_all('div', recursive=False)
     count = len(pages)
     if count >= 5:
         return 5
     else:
         return count
 
-print(get_page_count("python"))
 
 def extract_indeed_jobs(keyword):
     pages = get_page_count(keyword)
+    print("Found", pages, "pages")
     for page in range(pages):
-        base_url = "https://kr.indeed.com/jobs"
-        browser.get(f"{base_url}?q={keyword}")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome("./chromedriver.exe", options=options)
+        driver.get(f'https://kr.indeed.com/jobs?q={keyword}&start={page*10}')
+        finalurl = (f'https://kr.indeed.com/jobs?q={keyword}&start={page*10}')
+
+    print('requesting', finalurl)
+    if finalurl == None:
+        print('Cant request page')
+    else:
         results = []
-        soup = BeautifulSoup(browser.page_source, "html.parser")
-        job_list = soup.find("ul",class_="jobsearch-ResultsList")
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        job_list = soup.find('ul', class_='jobsearch-ResultsList')
         jobs = job_list.find_all('li', recursive=False)
         for job in jobs:
-            zone = job.find("div", class_="mosaic-zone")
+            zone = job.find('div', class_='mosaic-zone')
             if zone == None:
-                anchor = job.select_one("h2 a")
+                anchor = job.select_one('h2 a')
                 title = anchor['aria-label']
                 link = anchor['href']
-                company = job.find("span",class_="companyName")
-                location = job.find("div", class_="companyLocation")
+                company = job.find('span', class_='companyName')
+                location = job.find('div', class_='companyLocation')
                 job_data = {
-                    'link':f"https://kr.indeed.com{link}",
-                    'company':company.string,
-                    'location':location.string,
-                    'position':title
+                    'link': f'https://kr.indeed.com{link}',
+                    'company': company.string,
+                    'position': title,
+                    'location': location.string
                 }
-        results.append(job_data)
+    results.append(job_data)
+
+jobs = extract_indeed_jobs('python')
+print(jobs)
